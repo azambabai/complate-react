99170dcc1814d2bd00f98b37980de0084317e0ce
azambabaii <85355504+azambabai@users.noreply.github.com>
2022-08-09 20:48:12 +0430

    Update BlogList.js

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,22 @@
-const  BlogList = ({blogs , title , handleDelete}) => {
+// const  BlogList = ({blogs , title , handleDelete}) => {
+// //   const blogs = props.blogs;
+// //   const title = props.title;
+//     return (
+//     <div className="blog-list">
+//         <h2>{title}</h2>
+//            {blogs.map((blog) =>(
+//         <div className="blog-preview" key={blog.id}>
+//        <h2>{blog.title}</h2>
+//        <p>Written by  {blog.auther}</p>
+//        <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+//         </div>
+//            ))} 
+//     </div>
+//     )}
+// export default BlogList;
+
+
+const  BlogList = ({blogs , title}) => {
 //   const blogs = props.blogs;
 //   const title = props.title;
     return (
@@ -8,7 +26,7 @@ const  BlogList = ({blogs , title , handleDelete}) => {
         <div className="blog-preview" key={blog.id}>
        <h2>{blog.title}</h2>
        <p>Written by  {blog.auther}</p>
-       <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+
         </div>
            ))} 
     </div>
